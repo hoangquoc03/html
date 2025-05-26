c34a8d92e10dd7b06285f85eca8d5983368874f0
lequoc <lequoc150305@quoc150305gmail.com>
2025-05-26 08:45:20 +0700

    javascript2

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
-function solveEquation() {
-    let a = parseFloat(document.getElementById('a').value);
-    let b = parseFloat(document.getElementById('b').value);
-    let kq = "";
-
-    kq = "Phương trình: " + a + "x + " + b + " = 0<br>";
-
-    if (a === 0) {
-        if (b === 0) {
-            kq += "Phương trình vô số nghiệm.";
-        } else {
-            kq += "Phương trình vô nghiệm.";
-        }
-    } else {
-        let x = -b / a;
-        kq += "Có nghiệm: " + x;
-    }
-
-    document.getElementById('result').innerHTML = kq;
+function changeImg(element){
+    document.querySelector(".imgto").src = element.src;
+}
+function  resetImg(){
+    document.querySelector(".imgto").src = "./img/download (1).jpg";
 }
