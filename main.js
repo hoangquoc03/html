82c9bf1803f57d8e75d5d246b1d5802ef2c863bb
lequoc <lequoc150305@quoc150305gmail.com>
2025-05-26 09:17:13 +0700

    javascrip3

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function changeImg(element){
 function  resetImg(){
     document.querySelector(".imgto").src = "./img/download (1).jpg";
 }
+function showlightbox(){
+    document.querySelector('.cover-bg').style.display ="flex";
+}
+
+function hidelightbox(){
+    document.querySelector('.cover-bg').style.display ="none";
+}
